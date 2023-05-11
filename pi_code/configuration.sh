24ff3661dc248b5b89cf5bb9169dced3ae33aade
Raspberry <pi@raspberry.com>
2023-05-11 10:22:34 +0200

    Added configuration file.

---
+++ pi_code/configuration.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#***********************************************
+# This script defines constants and settings for
+# the radish waterer program
+#***********************************************
+
+# Setup for the various radish plants
+N_RADISHES=1
+REMOTE_IPS=(
+    "10.42.0.2"
+)
+CONTROLLER_SERIALS=(
+    "/dev/ttyACM0"
+)

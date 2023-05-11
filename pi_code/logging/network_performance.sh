43af9d2dc7aaf498a1015dc5c5ca1c3cd80ccabd
Raspberry <pi@raspberry.com>
2023-05-11 13:19:36 +0200

    Added script for network monitoring.

--- pi_code/logging/network_performance.sh
+++ pi_code/logging/network_performance.sh
@@ -10,5 +10,13 @@ wlan0_line=($(cat /proc/net/dev | grep "wlan0"))
 
 # Extract bytes sent/received
 eth0_rx=${eth0_line[1]}
-eth0_tx=${eth0_line[1]}
-echo $eth0_rx
+eth0_tx=${eth0_line[9]}
+
+wlan0_rx=${wlan0_line[1]}
+wlan0_tx=${wlan0_line[9]}
+
+# Return results
+echo "eth0 RX: $eth0_rx"
+echo "eth0 TX: $eth0_tx"
+echo "wlan0 RX: $wlan0_rx"
+echo "wlan0 TX: $wlan0_tx"
